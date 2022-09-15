57fdd0df91dbd824b47caa00106043dab0a08324
Dario D <57863042+nekonikoban@users.noreply.github.com>
2022-09-15 18:17:12 +0200

    Interactive Avatar
    Possible for kusers to upload their own avatar from now on

--- js/script.js
+++ js/script.js
@@ -31,6 +31,27 @@ $(document).ready(function() {
 	const fadeSpeed = 700
 	const scrollSpeed = 700
 	//PAGE LOADING
+
+	//AVATAR
+	var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.profile-pic').attr('src', e.target.result);
+            }
+    
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+   
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    })
+    
+    $(".upload-button").on('click', function() {
+       $(".file-upload").click();
+    })
 	
 	//SIDEBAR
 	function initUserInfo() {
